c80896b31d449a96d24a8b92e4001383473b5d3d
John Doe <webhelpadmin@gmail.com>
2016-10-31 11:50:25 +0200

    1 - add check if device is connected to the internet

--- HFNotification/MainActivity.cs
+++ HFNotification/MainActivity.cs
@@ -1,6 +1,10 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using System;
+using Android.Content;
+using Java.Lang;
+using System.Net;
 
 namespace HFNotification
 {
@@ -10,10 +14,64 @@ namespace HFNotification
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
-
+            //Check if Internet is available
+            if (isOnline())
+            {
+                // code here
+            }
+            else
+            {
+                // code
+            }
             // Set our view from the "main" layout resource
             // SetContentView (Resource.Layout.Main);
         }
+
+        //Check if Internet is available
+        public bool isOnline()
+        {
+            Runtime runtime = Runtime.GetRuntime();
+            try
+            {
+                Java.Lang.Process ipProcess = runtime.Exec("/system/bin/ping -c 1 8.8.8.8");
+                int exitValue = ipProcess.WaitFor();
+                return (exitValue == 0);
+            }
+            catch (Java.IO.IOException e) { e.PrintStackTrace(); }
+            catch (InterruptedException e) { e.PrintStackTrace(); }
+
+            return false;
+        }
+
+        // Another method to check if Internet is available
+        public bool CheckInternetConnection()
+        {
+            string CheckUrl = "http://google.com";
+
+            try
+            {
+                HttpWebRequest iNetRequest = (HttpWebRequest)WebRequest.Create(CheckUrl);
+
+                iNetRequest.Timeout = 5000;
+
+                WebResponse iNetResponse = iNetRequest.GetResponse();
+
+                // Console.WriteLine ("...connection established..." + iNetRequest.ToString ());
+                iNetResponse.Close();
+
+                return true;
+
+            }
+            catch (WebException ex)
+            {
+
+                // Console.WriteLine (".....no connection..." + ex.ToString ());
+
+                return false;
+            }
+        }
+
+
     }
 }
 
