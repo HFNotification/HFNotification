d481a3f9d1d4492544e86e8717280f120256965c
John Doe <webhelpadmin@gmail.com>
2016-11-01 20:23:32 +0200

    1 - clean MainActivity.cs and change to v7.0

--- HFNotification/MainActivity.cs
+++ HFNotification/MainActivity.cs
@@ -16,68 +16,8 @@ namespace HFNotification
 		{
 			base.OnCreate(bundle);
 
-			
 			// Set our view from the "main" layout resource
 			SetContentView(Resource.Layout.Main);
-			//Check if Internet is available
-
-			if (isOnline())
-			{
-				//Test if GPS is available
-				msgText = FindViewById<TextView>(Resource.Id.msgText);
-				if (IsPlayServicesAvailable())
-				{
-					if (!LoginService.Login())
-					{
-						StartActivity(typeof(LoginActivity));
-						Finish();
-					}
-
-				}
-
-
-			}
-			else
-			{
-				// code
-				//MessageBox.Show("Internet connections are not available");
-			}
-
-
-
-
 		}
-
-
-		//Check if Internet is available
-		public bool isOnline()
-		{
-			ConnectivityManager connectivityManager = (ConnectivityManager)GetSystemService(ConnectivityService);
-			NetworkInfo activeConnection = connectivityManager.ActiveNetworkInfo;
-			return (activeConnection != null) && activeConnection.IsConnected;
-		}
-
-		public bool IsPlayServicesAvailable()
-		{
-			int resultCode = GoogleApiAvailability.Instance.IsGooglePlayServicesAvailable(this);
-			if (resultCode != ConnectionResult.Success)
-			{
-				if (GoogleApiAvailability.Instance.IsUserResolvableError(resultCode))
-					msgText.Text = GoogleApiAvailability.Instance.GetErrorString(resultCode);
-				else
-				{
-					msgText.Text = "Sorry, this device is not supported";
-					Finish();
-				}
-				return false;
-			}
-			else
-			{
-				msgText.Text = "Google Play Services is available.";
-				return true;
-			}
-		}
-
-
 	}
 }
