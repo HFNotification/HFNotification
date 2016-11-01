2d65709c9274f1c0a24a7807dcae2f4e8e39bc94
John Doe <webhelpadmin@gmail.com>
2016-11-01 17:25:38 +0200

    1 - fixed build solution

--- HFNotification/Resources/Resource.Designer.cs
+++ HFNotification/Resources/Resource.Designer.cs
@@ -31,6 +31,24 @@ namespace HFNotification
 		public partial class Attribute
 		{
 			
+			// aapt resource value: 0x7f010003
+			public const int buttonSize = 2130771971;
+			
+			// aapt resource value: 0x7f010002
+			public const int circleCrop = 2130771970;
+			
+			// aapt resource value: 0x7f010004
+			public const int colorScheme = 2130771972;
+			
+			// aapt resource value: 0x7f010001
+			public const int imageAspectRatio = 2130771969;
+			
+			// aapt resource value: 0x7f010000
+			public const int imageAspectRatioAdjust = 2130771968;
+			
+			// aapt resource value: 0x7f010005
+			public const int scopeUris = 2130771973;
+			
 			static Attribute()
 			{
 				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
@@ -44,11 +62,71 @@ namespace HFNotification
 		public partial class Color
 		{
 			
-			// aapt resource value: 0x7f050000
-			public const int theme_custom_background = 2131034112;
+			// aapt resource value: 0x7f060012
+			public const int common_google_signin_btn_text_dark = 2131099666;
 			
-			// aapt resource value: 0x7f050001
-			public const int theme_devicedefault_background = 2131034113;
+			// aapt resource value: 0x7f060000
+			public const int common_google_signin_btn_text_dark_default = 2131099648;
+			
+			// aapt resource value: 0x7f060001
+			public const int common_google_signin_btn_text_dark_disabled = 2131099649;
+			
+			// aapt resource value: 0x7f060002
+			public const int common_google_signin_btn_text_dark_focused = 2131099650;
+			
+			// aapt resource value: 0x7f060003
+			public const int common_google_signin_btn_text_dark_pressed = 2131099651;
+			
+			// aapt resource value: 0x7f060013
+			public const int common_google_signin_btn_text_light = 2131099667;
+			
+			// aapt resource value: 0x7f060004
+			public const int common_google_signin_btn_text_light_default = 2131099652;
+			
+			// aapt resource value: 0x7f060005
+			public const int common_google_signin_btn_text_light_disabled = 2131099653;
+			
+			// aapt resource value: 0x7f060006
+			public const int common_google_signin_btn_text_light_focused = 2131099654;
+			
+			// aapt resource value: 0x7f060007
+			public const int common_google_signin_btn_text_light_pressed = 2131099655;
+			
+			// aapt resource value: 0x7f060014
+			public const int common_plus_signin_btn_text_dark = 2131099668;
+			
+			// aapt resource value: 0x7f060008
+			public const int common_plus_signin_btn_text_dark_default = 2131099656;
+			
+			// aapt resource value: 0x7f060009
+			public const int common_plus_signin_btn_text_dark_disabled = 2131099657;
+			
+			// aapt resource value: 0x7f06000a
+			public const int common_plus_signin_btn_text_dark_focused = 2131099658;
+			
+			// aapt resource value: 0x7f06000b
+			public const int common_plus_signin_btn_text_dark_pressed = 2131099659;
+			
+			// aapt resource value: 0x7f060015
+			public const int common_plus_signin_btn_text_light = 2131099669;
+			
+			// aapt resource value: 0x7f06000c
+			public const int common_plus_signin_btn_text_light_default = 2131099660;
+			
+			// aapt resource value: 0x7f06000d
+			public const int common_plus_signin_btn_text_light_disabled = 2131099661;
+			
+			// aapt resource value: 0x7f06000e
+			public const int common_plus_signin_btn_text_light_focused = 2131099662;
+			
+			// aapt resource value: 0x7f06000f
+			public const int common_plus_signin_btn_text_light_pressed = 2131099663;
+			
+			// aapt resource value: 0x7f060010
+			public const int theme_custom_background = 2131099664;
+			
+			// aapt resource value: 0x7f060011
+			public const int theme_devicedefault_background = 2131099665;
 			
 			static Color()
 			{
@@ -64,10 +142,136 @@ namespace HFNotification
 		{
 			
 			// aapt resource value: 0x7f020000
-			public const int Icon = 2130837504;
+			public const int common_full_open_on_phone = 2130837504;
 			
 			// aapt resource value: 0x7f020001
-			public const int startprogressbar = 2130837505;
+			public const int common_google_signin_btn_icon_dark = 2130837505;
+			
+			// aapt resource value: 0x7f020002
+			public const int common_google_signin_btn_icon_dark_disabled = 2130837506;
+			
+			// aapt resource value: 0x7f020003
+			public const int common_google_signin_btn_icon_dark_focused = 2130837507;
+			
+			// aapt resource value: 0x7f020004
+			public const int common_google_signin_btn_icon_dark_normal = 2130837508;
+			
+			// aapt resource value: 0x7f020005
+			public const int common_google_signin_btn_icon_dark_pressed = 2130837509;
+			
+			// aapt resource value: 0x7f020006
+			public const int common_google_signin_btn_icon_light = 2130837510;
+			
+			// aapt resource value: 0x7f020007
+			public const int common_google_signin_btn_icon_light_disabled = 2130837511;
+			
+			// aapt resource value: 0x7f020008
+			public const int common_google_signin_btn_icon_light_focused = 2130837512;
+			
+			// aapt resource value: 0x7f020009
+			public const int common_google_signin_btn_icon_light_normal = 2130837513;
+			
+			// aapt resource value: 0x7f02000a
+			public const int common_google_signin_btn_icon_light_pressed = 2130837514;
+			
+			// aapt resource value: 0x7f02000b
+			public const int common_google_signin_btn_text_dark = 2130837515;
+			
+			// aapt resource value: 0x7f02000c
+			public const int common_google_signin_btn_text_dark_disabled = 2130837516;
+			
+			// aapt resource value: 0x7f02000d
+			public const int common_google_signin_btn_text_dark_focused = 2130837517;
+			
+			// aapt resource value: 0x7f02000e
+			public const int common_google_signin_btn_text_dark_normal = 2130837518;
+			
+			// aapt resource value: 0x7f02000f
+			public const int common_google_signin_btn_text_dark_pressed = 2130837519;
+			
+			// aapt resource value: 0x7f020010
+			public const int common_google_signin_btn_text_light = 2130837520;
+			
+			// aapt resource value: 0x7f020011
+			public const int common_google_signin_btn_text_light_disabled = 2130837521;
+			
+			// aapt resource value: 0x7f020012
+			public const int common_google_signin_btn_text_light_focused = 2130837522;
+			
+			// aapt resource value: 0x7f020013
+			public const int common_google_signin_btn_text_light_normal = 2130837523;
+			
+			// aapt resource value: 0x7f020014
+			public const int common_google_signin_btn_text_light_pressed = 2130837524;
+			
+			// aapt resource value: 0x7f020015
+			public const int common_ic_googleplayservices = 2130837525;
+			
+			// aapt resource value: 0x7f020016
+			public const int common_plus_signin_btn_icon_dark = 2130837526;
+			
+			// aapt resource value: 0x7f020017
+			public const int common_plus_signin_btn_icon_dark_disabled = 2130837527;
+			
+			// aapt resource value: 0x7f020018
+			public const int common_plus_signin_btn_icon_dark_focused = 2130837528;
+			
+			// aapt resource value: 0x7f020019
+			public const int common_plus_signin_btn_icon_dark_normal = 2130837529;
+			
+			// aapt resource value: 0x7f02001a
+			public const int common_plus_signin_btn_icon_dark_pressed = 2130837530;
+			
+			// aapt resource value: 0x7f02001b
+			public const int common_plus_signin_btn_icon_light = 2130837531;
+			
+			// aapt resource value: 0x7f02001c
+			public const int common_plus_signin_btn_icon_light_disabled = 2130837532;
+			
+			// aapt resource value: 0x7f02001d
+			public const int common_plus_signin_btn_icon_light_focused = 2130837533;
+			
+			// aapt resource value: 0x7f02001e
+			public const int common_plus_signin_btn_icon_light_normal = 2130837534;
+			
+			// aapt resource value: 0x7f02001f
+			public const int common_plus_signin_btn_icon_light_pressed = 2130837535;
+			
+			// aapt resource value: 0x7f020020
+			public const int common_plus_signin_btn_text_dark = 2130837536;
+			
+			// aapt resource value: 0x7f020021
+			public const int common_plus_signin_btn_text_dark_disabled = 2130837537;
+			
+			// aapt resource value: 0x7f020022
+			public const int common_plus_signin_btn_text_dark_focused = 2130837538;
+			
+			// aapt resource value: 0x7f020023
+			public const int common_plus_signin_btn_text_dark_normal = 2130837539;
+			
+			// aapt resource value: 0x7f020024
+			public const int common_plus_signin_btn_text_dark_pressed = 2130837540;
+			
+			// aapt resource value: 0x7f020025
+			public const int common_plus_signin_btn_text_light = 2130837541;
+			
+			// aapt resource value: 0x7f020026
+			public const int common_plus_signin_btn_text_light_disabled = 2130837542;
+			
+			// aapt resource value: 0x7f020027
+			public const int common_plus_signin_btn_text_light_focused = 2130837543;
+			
+			// aapt resource value: 0x7f020028
+			public const int common_plus_signin_btn_text_light_normal = 2130837544;
+			
+			// aapt resource value: 0x7f020029
+			public const int common_plus_signin_btn_text_light_pressed = 2130837545;
+			
+			// aapt resource value: 0x7f02002a
+			public const int Icon = 2130837546;
+			
+			// aapt resource value: 0x7f02002b
+			public const int startprogressbar = 2130837547;
 			
 			static Drawable()
 			{
@@ -82,29 +286,56 @@ namespace HFNotification
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f070002
-			public const int email = 2131165186;
+			// aapt resource value: 0x7f080000
+			public const int adjust_height = 2131230720;
 			
-			// aapt resource value: 0x7f070000
-			public const int linearLayout1 = 2131165184;
+			// aapt resource value: 0x7f080001
+			public const int adjust_width = 2131230721;
 			
-			// aapt resource value: 0x7f070005
-			public const int loginbutton = 2131165189;
+			// aapt resource value: 0x7f080006
+			public const int auto = 2131230726;
 			
-			// aapt resource value: 0x7f070006
-			public const int msgText = 2131165190;
+			// aapt resource value: 0x7f080007
+			public const int dark = 2131230727;
 			
-			// aapt resource value: 0x7f070004
-			public const int password = 2131165188;
+			// aapt resource value: 0x7f08000b
+			public const int email = 2131230731;
 			
-			// aapt resource value: 0x7f070007
-			public const int startprogressbar = 2131165191;
+			// aapt resource value: 0x7f080003
+			public const int icon_only = 2131230723;
 			
-			// aapt resource value: 0x7f070003
-			public const int textView1 = 2131165187;
+			// aapt resource value: 0x7f080008
+			public const int light = 2131230728;
 			
-			// aapt resource value: 0x7f070001
-			public const int textView2 = 2131165185;
+			// aapt resource value: 0x7f080009
+			public const int linearLayout1 = 2131230729;
+			
+			// aapt resource value: 0x7f08000e
+			public const int loginbutton = 2131230734;
+			
+			// aapt resource value: 0x7f08000f
+			public const int msgText = 2131230735;
+			
+			// aapt resource value: 0x7f080002
+			public const int none = 2131230722;
+			
+			// aapt resource value: 0x7f08000d
+			public const int password = 2131230733;
+			
+			// aapt resource value: 0x7f080004
+			public const int standard = 2131230724;
+			
+			// aapt resource value: 0x7f080010
+			public const int startprogressbar = 2131230736;
+			
+			// aapt resource value: 0x7f08000c
+			public const int textView1 = 2131230732;
+			
+			// aapt resource value: 0x7f08000a
+			public const int textView2 = 2131230730;
+			
+			// aapt resource value: 0x7f080005
+			public const int wide = 2131230725;
 			
 			static Id()
 			{
@@ -112,6 +343,22 @@ namespace HFNotification
 			}
 			
 			private Id()
+			{
+			}
+		}
+		
+		public partial class Integer
+		{
+			
+			// aapt resource value: 0x7f040000
+			public const int google_play_services_version = 2130968576;
+			
+			static Integer()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Integer()
 			{
 			}
 		}
@@ -141,29 +388,89 @@ namespace HFNotification
 		public partial class String
 		{
 			
-			// aapt resource value: 0x7f040007
-			public const int ApplicationName = 2130968583;
+			// aapt resource value: 0x7f05001b
+			public const int ApplicationName = 2131034139;
 			
-			// aapt resource value: 0x7f040006
-			public const int Hello = 2130968582;
+			// aapt resource value: 0x7f05001a
+			public const int Hello = 2131034138;
 			
-			// aapt resource value: 0x7f040002
-			public const int default_web_client_id = 2130968578;
+			// aapt resource value: 0x7f050001
+			public const int common_google_play_services_enable_button = 2131034113;
 			
-			// aapt resource value: 0x7f040003
-			public const int firebase_database_url = 2130968579;
+			// aapt resource value: 0x7f050002
+			public const int common_google_play_services_enable_text = 2131034114;
 			
-			// aapt resource value: 0x7f040000
-			public const int gcm_defaultSenderId = 2130968576;
+			// aapt resource value: 0x7f050003
+			public const int common_google_play_services_enable_title = 2131034115;
 			
-			// aapt resource value: 0x7f040004
-			public const int google_api_key = 2130968580;
+			// aapt resource value: 0x7f050004
+			public const int common_google_play_services_install_button = 2131034116;
 			
-			// aapt resource value: 0x7f040001
-			public const int google_app_id = 2130968577;
+			// aapt resource value: 0x7f050005
+			public const int common_google_play_services_install_text_phone = 2131034117;
 			
-			// aapt resource value: 0x7f040005
-			public const int google_crash_reporting_api_key = 2130968581;
+			// aapt resource value: 0x7f050006
+			public const int common_google_play_services_install_text_tablet = 2131034118;
+			
+			// aapt resource value: 0x7f050007
+			public const int common_google_play_services_install_title = 2131034119;
+			
+			// aapt resource value: 0x7f050008
+			public const int common_google_play_services_notification_ticker = 2131034120;
+			
+			// aapt resource value: 0x7f050000
+			public const int common_google_play_services_unknown_issue = 2131034112;
+			
+			// aapt resource value: 0x7f050009
+			public const int common_google_play_services_unsupported_text = 2131034121;
+			
+			// aapt resource value: 0x7f05000a
+			public const int common_google_play_services_unsupported_title = 2131034122;
+			
+			// aapt resource value: 0x7f05000b
+			public const int common_google_play_services_update_button = 2131034123;
+			
+			// aapt resource value: 0x7f05000c
+			public const int common_google_play_services_update_text = 2131034124;
+			
+			// aapt resource value: 0x7f05000d
+			public const int common_google_play_services_update_title = 2131034125;
+			
+			// aapt resource value: 0x7f05000e
+			public const int common_google_play_services_updating_text = 2131034126;
+			
+			// aapt resource value: 0x7f05000f
+			public const int common_google_play_services_updating_title = 2131034127;
+			
+			// aapt resource value: 0x7f050010
+			public const int common_google_play_services_wear_update_text = 2131034128;
+			
+			// aapt resource value: 0x7f050011
+			public const int common_open_on_phone = 2131034129;
+			
+			// aapt resource value: 0x7f050012
+			public const int common_signin_button_text = 2131034130;
+			
+			// aapt resource value: 0x7f050013
+			public const int common_signin_button_text_long = 2131034131;
+			
+			// aapt resource value: 0x7f050016
+			public const int default_web_client_id = 2131034134;
+			
+			// aapt resource value: 0x7f050017
+			public const int firebase_database_url = 2131034135;
+			
+			// aapt resource value: 0x7f050014
+			public const int gcm_defaultSenderId = 2131034132;
+			
+			// aapt resource value: 0x7f050018
+			public const int google_api_key = 2131034136;
+			
+			// aapt resource value: 0x7f050015
+			public const int google_app_id = 2131034133;
+			
+			// aapt resource value: 0x7f050019
+			public const int google_crash_reporting_api_key = 2131034137;
 			
 			static String()
 			{
@@ -178,8 +485,8 @@ namespace HFNotification
 		public partial class Style
 		{
 			
-			// aapt resource value: 0x7f060000
-			public const int Theme_Custom = 2131099648;
+			// aapt resource value: 0x7f070000
+			public const int Theme_Custom = 2131165184;
 			
 			static Style()
 			{
@@ -187,6 +494,47 @@ namespace HFNotification
 			}
 			
 			private Style()
+			{
+			}
+		}
+		
+		public partial class Styleable
+		{
+			
+			public static int[] LoadingImageView = new int[] {
+					2130771968,
+					2130771969,
+					2130771970};
+			
+			// aapt resource value: 2
+			public const int LoadingImageView_circleCrop = 2;
+			
+			// aapt resource value: 1
+			public const int LoadingImageView_imageAspectRatio = 1;
+			
+			// aapt resource value: 0
+			public const int LoadingImageView_imageAspectRatioAdjust = 0;
+			
+			public static int[] SignInButton = new int[] {
+					2130771971,
+					2130771972,
+					2130771973};
+			
+			// aapt resource value: 0
+			public const int SignInButton_buttonSize = 0;
+			
+			// aapt resource value: 1
+			public const int SignInButton_colorScheme = 1;
+			
+			// aapt resource value: 2
+			public const int SignInButton_scopeUris = 2;
+			
+			static Styleable()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Styleable()
 			{
 			}
 		}
