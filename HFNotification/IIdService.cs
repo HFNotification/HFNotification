e7bfc36fe916b9ade37f5f9cbce963c3c20f69d9
John Doe <webhelpadmin@gmail.com>
2016-11-01 11:01:22 +0200

    1 - add tab spaces

--- HFNotification/IIdService.cs
+++ HFNotification/IIdService.cs
@@ -5,32 +5,32 @@ using System.Net;
 
 namespace HFNotification
 {
-    [Service]
-    [IntentFilter(new[] { "com.google.firebase.INSTANCE_ID_EVENT" })]
-    public class IIDService : FirebaseInstanceIdService
-    {
-        const string TAG = "IIDService";
+	[Service]
+	[IntentFilter(new[] { "com.google.firebase.INSTANCE_ID_EVENT" })]
+	public class IIDService : FirebaseInstanceIdService
+	{
+		const string TAG = "IIDService";
 
-        /**
+		/**
          * Called if InstanceID token is updated. This may occur if the security of
          * the previous token had been compromised
          */
-        public override void OnTokenRefresh()
-        {
-            // Get updated InstanceID token.
-            // TODO: Implement this method to send any registration to your app's servers.
-            var refreshedToken = FirebaseInstanceId.Instance.Token;
-            Android.Util.Log.Debug(TAG, "Refreshed token: " + refreshedToken);
-            SendRegistrationToServer(FirebaseInstanceId.Instance.Token);
-        }
+		public override void OnTokenRefresh()
+		{
+			// Get updated InstanceID token.
+			// TODO: Implement this method to send any registration to your app's servers.
+			var refreshedToken = FirebaseInstanceId.Instance.Token;
+			Android.Util.Log.Debug(TAG, "Refreshed token: " + refreshedToken);
+			SendRegistrationToServer(FirebaseInstanceId.Instance.Token);
+		}
 
-        void SendRegistrationToServer(string token)
-        {
-            //TODO: (!Dont send token from here in future ) add login check.
-            var httpWebRequest = (HttpWebRequest)WebRequest.Create("http://requestb.in/14q7hw11");
-            httpWebRequest.Headers.Add("Token:" + token);
-            httpWebRequest.Method = "POST";
-            var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
-        }
-    }
+		void SendRegistrationToServer(string token)
+		{
+			//TODO: (!Dont send token from here in future ) add login check.
+			var httpWebRequest = (HttpWebRequest)WebRequest.Create("http://requestb.in/14q7hw11");
+			httpWebRequest.Headers.Add("Token:" + token);
+			httpWebRequest.Method = "POST";
+			var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+		}
+	}
 }
--- HFNotification/MainActivity.cs
+++ HFNotification/MainActivity.cs
@@ -8,63 +8,63 @@ using Android.Util;
 
 namespace HFNotification
 {
-    [Activity(Label = "@string/ApplicationName", MainLauncher = true, Icon = "@drawable/icon")]
-    public class MainActivity : Activity
-    {
-        const string TAG = "MainActivity";
-        TextView msgText;
-        protected override void OnCreate(Bundle bundle)
-        {
-            base.OnCreate(bundle);
-            //Check if Internet is available
-            if (isOnline())
-            {
-                // code here
-            }
-            else
-            {
-                // code
-                //MessageBox.Show("Internet connections are not available");
-            }
-            // Set our view from the "main" layout resource
-            SetContentView(Resource.Layout.Main);
+	[Activity(Label = "@string/ApplicationName", MainLauncher = true, Icon = "@drawable/icon")]
+	public class MainActivity : Activity
+	{
+		const string TAG = "MainActivity";
+		TextView msgText;
+		protected override void OnCreate(Bundle bundle)
+		{
+			base.OnCreate(bundle);
+			//Check if Internet is available
+			if (isOnline())
+			{
+				// code here
+			}
+			else
+			{
+				// code
+				//MessageBox.Show("Internet connections are not available");
+			}
+			// Set our view from the "main" layout resource
+			SetContentView(Resource.Layout.Main);
 
-            //Test if GPS is available
-            msgText = FindViewById<TextView>(Resource.Id.msgText);
+			//Test if GPS is available
+			msgText = FindViewById<TextView>(Resource.Id.msgText);
 
-            IsPlayServicesAvailable();
-        }
-
-
-        //Check if Internet is available
-        public bool isOnline()
-        {
-            ConnectivityManager connectivityManager = (ConnectivityManager)GetSystemService(ConnectivityService);
-            NetworkInfo activeConnection = connectivityManager.ActiveNetworkInfo;
-            return (activeConnection != null) && activeConnection.IsConnected;
-        }
-
-        public bool IsPlayServicesAvailable()
-        {
-            int resultCode = GoogleApiAvailability.Instance.IsGooglePlayServicesAvailable(this);
-            if (resultCode != ConnectionResult.Success)
-            {
-                if (GoogleApiAvailability.Instance.IsUserResolvableError(resultCode))
-                    msgText.Text = GoogleApiAvailability.Instance.GetErrorString(resultCode);
-                else
-                {
-                    msgText.Text = "Sorry, this device is not supported";
-                    Finish();
-                }
-                return false;
-            }
-            else
-            {
-                msgText.Text = "Google Play Services is available.";
-                return true;
-            }
-        }
+			IsPlayServicesAvailable();
+		}
 
 
-    }
+		//Check if Internet is available
+		public bool isOnline()
+		{
+			ConnectivityManager connectivityManager = (ConnectivityManager)GetSystemService(ConnectivityService);
+			NetworkInfo activeConnection = connectivityManager.ActiveNetworkInfo;
+			return (activeConnection != null) && activeConnection.IsConnected;
+		}
+
+		public bool IsPlayServicesAvailable()
+		{
+			int resultCode = GoogleApiAvailability.Instance.IsGooglePlayServicesAvailable(this);
+			if (resultCode != ConnectionResult.Success)
+			{
+				if (GoogleApiAvailability.Instance.IsUserResolvableError(resultCode))
+					msgText.Text = GoogleApiAvailability.Instance.GetErrorString(resultCode);
+				else
+				{
+					msgText.Text = "Sorry, this device is not supported";
+					Finish();
+				}
+				return false;
+			}
+			else
+			{
+				msgText.Text = "Google Play Services is available.";
+				return true;
+			}
+		}
+
+
+	}
 }
